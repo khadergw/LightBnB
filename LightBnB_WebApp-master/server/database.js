1d8a20a04a12fc4600c31e829c14aed1a5f1a9cf
GHadeer Khader <qods_a@yahoo.com>
2022-01-04 19:12:11 +0000

    add a property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -172,10 +172,19 @@ const getAllProperties = (options, limit = 10) => {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+ const values = [property.title, property.description, Number(property.number_of_bedrooms), Number(property.number_of_bathrooms), Number(property.parking_spaces), Number(property.cost_per_night), property.thumbnail_photo_url, property.cover_photo_url, property.street, property.country, property.city, property.province, property.post_code, property.owner_id];
+  
+
+  const queryString = `
+  INSERT INTO properties (title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url, cover_photo_url, street, country, city, province, post_code, owner_id )
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`
+  ;
+ 
+
+  return pool.query(queryString, values)
+    .then(res => res.rows);
+
 }
 exports.addProperty = addProperty;
 
